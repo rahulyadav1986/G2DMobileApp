143617c7e13dae04742441214616f264b490fe5e
rahulyadav1986 <rahullooksforward@gmail.com>
2022-08-20 20:21:33 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'g2d',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
